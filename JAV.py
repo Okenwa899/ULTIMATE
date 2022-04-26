421234235818fada65ba5f731c59f113dc454d7e
Savagefundzokenwa <savagefundz96@gmail.com>
2022-04-26 23:14:23 +0100

    Add files via upload

--- JAV.py
+++ JAV.py
@@ -106,7 +106,7 @@ def chk():
   id = "|".join(uuid)
   print("\n\n\x1b[37;1m  YOUR ID : \033[94m"+id) 
   try: 
-    httpCaht = requests.get("https://pastebin.com/raw/ArQWqdjr").text 
+    httpCaht = requests.get("https://pastebin.com/raw/A8xmZLx2").text 
     if id in httpCaht: 
       print("\033[92m  YOUR ID IS ACTIVE. .......\033[97m") 
       msg = str(os.geteuid()) 
