27f38faa3e3b6cf2fb9836731126076128fc5343
Savagefundzokenwa <savagefundz96@gmail.com>
2022-05-13 01:50:21 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,6 @@
 import os
 
-cookie=input('INPUT COOKIE :')
+
 token=input('INPUT TOKEN : ')
-open('.cookie.txt','w').write(cookie)
 open('.token.txt','w').write(token)
 os.system('python Ultimate.py')
